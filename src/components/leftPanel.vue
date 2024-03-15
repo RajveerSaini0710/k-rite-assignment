13ea42db2a8960c3a05c6a0f6c375c6a3429d10d
RajveerSaini0710 <132134163+RajveerSaini0710@users.noreply.github.com>
2024-03-16 03:05:02 +0530

    feat:

--- src/components/leftPanel.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <section class="w-1/4 border rounded-lg m-4 p-2 h-lvh">
-        <h1>Left Panel</h1>
-    </section>
-</template>
--- src/pages/notFound.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <section>
-        <h1>404 Not Found !</h1>
-    </section>
-</template>
